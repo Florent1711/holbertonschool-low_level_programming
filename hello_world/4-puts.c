bfbaa1c75325719601b3c8d8c97f11820557eba4
Florent1711 <graphit01@hotmail.com>
2023-10-12 03:32:39 -0700

    C - Hello, World

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int main (void)
-{
-  puts( "Programming is like building a multilingual puzzle" );
+/**
+ * main - Entry point
+ *
+ * Description: 'Affiche une chaine de caractères'
+ *
+ * Return: Always 0 (Success)
+ */
 
-  return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
