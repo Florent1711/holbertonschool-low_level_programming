f097e9db2556572ee45ca3299dfdb7d06459264d
Florent1711 <graphit01@hotmail.com>
2023-10-12 03:52:02 -0700

    C - Hello, World

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Affiche une chaine de caractère avec printf'
+ *
+ * Return: Always 0 (success)
+ */
+
+int main ()
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+}
