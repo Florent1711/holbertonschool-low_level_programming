cc86ad5237e2a48d16fd352114c08f0ea70d3f98
Florent1711 <graphit01@hotmail.com>
2023-10-16 01:42:38 -0700

    C - Variables, if, else, while

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Teste si trage aléatoire est positif, négatif ou zero
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	if (n == 0)
+	{
+		printf("%d is zero \n", n);
+	}
+
+	else if (n < 0)
+	{
+		printf("%d is negative \n", n);
+	}
+
+	else
+	{
+		printf("%d is positive \n", n);
+	}
+
+	return (0);
+}
