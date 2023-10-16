5c0e08e829916c3e87fb82eedcf9e32ba5e577e1
Florent1711 <graphit01@hotmail.com>
2023-10-16 05:12:52 -0700

    C - Variables, if, else, while

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * Description: 'Affiche l'alphabet avec putchar'
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
-	int letter;
+	char letter;
 
-	for (letter = 97; letter == 122; letter++)
-	{
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar (letter);
-	}
-
 	putchar ('\n');
 
 	return (0);
