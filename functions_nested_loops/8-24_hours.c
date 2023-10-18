6af7f70bf5c037cce1b48234f6f1045bad7d692f
Florent1711 <graphit01@hotmail.com>
2023-10-18 03:17:23 -0700

    C - Functions, nested loops

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - principal
+ *
+ * Description: fonction qui écrit chaque minute d'une journée
+ *
+ *
+ */
+
+void jack_bauer(void)
+{
+	int heures;
+	int minutes;
+
+	for (heures = 0; heures <= 23; heures++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar((heures / 10) + '0');
+			_putchar((heures % 10) + '0'OB);
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
+
+}
