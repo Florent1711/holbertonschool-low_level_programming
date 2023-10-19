8db05cfd0f85e5edcfd1a58df4d21ffe02d0a5cc
Florent1711 <graphit01@hotmail.com>
2023-10-19 05:38:32 -0700

    C - Functions, nested loops

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,9 +1,16 @@
 #include "main.h"
 
+/**
+ * add - principal
+ *
+ * Description: fonction qui additionne 2 entiers et renvoie le resultat
+ *
+ * @nombre1: valeur 1
+ * @nombre2: valeur 2
+ * Return: nombre1, nombre2
+ */
+
 int add(int nombre1, int nombre2)
 {
-	int somme;
-	somme = (nombre1 + nombre2);
-
-	return (somme);
+	return (nombre1 + nombre2);
 }
