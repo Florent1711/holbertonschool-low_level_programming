ca69d33957c7508bf85fff9b7d99fe38b04f46ee
Florent1711 <graphit01@hotmail.com>
2023-10-21 07:42:05 -0700

    C - More functions, more nested loops

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Principal
+ *
+ *  Description: affiche les nombres de 0 à 9, sans 2 et 4
+ */
+
+
+void print_most_numbers(void)
+{
+	int chiffre;
+
+	for (chiffre = '0'; chiffre <= '9'; chiffre++)
+	{
+		if (chiffre == '2' || chiffre == '4')
+			continue;
+		else
+			_putchar(chiffre);
+	}
+}
