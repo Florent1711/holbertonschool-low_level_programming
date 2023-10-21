edbeae3c4e5c1ef3375fde6509b885d3027bbfad
Florent1711 <graphit01@hotmail.com>
2023-10-21 09:24:27 -0700

    C - More functions, more nested loops

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * print_line - Principal
+ * @n: nombre de _ à afficher
+ *
+ * Description: affiche un underscor tant que n n'est pas 0
+ */
+
 void print_line(int n)
 {
 	while (n-- > 0)
