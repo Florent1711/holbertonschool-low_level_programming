bf669e26c4a81616ce7e2a84aa4e87f5c3323a3a
Florent1711 <graphit01@hotmail.com>
2023-10-21 07:11:08 -0700

    C - More functions, more nested loops

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,19 @@
 #include "main.h"
 
-
+/**
+ * print_numbers - principal
+ *
+ * Description: affiche les nombres de 0 à 9
+ */
 
 void print_numbers(void)
 {
 	int chiffre;
 
-	for (chiffre = 0; chiffre <= 9; chiffre++)
+	for (chiffre = '0'; chiffre <= '9'; chiffre++)
 	{
 		_putchar(chiffre);
 	}
+
+	_putchar('\n');
 }
