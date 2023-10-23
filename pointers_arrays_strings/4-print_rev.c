8d9160fd4a5d745635e3a213be55eebabf934c3e
Florent1711 <graphit01@hotmail.com>
2023-10-23 11:38:07 -0700

    C - Pointers, arrays and strings

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void print_rev(char *s)
+{
+	int compte;
+	int i;
+	int r;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		compte++;
+	}
+	for (r = compte; r >= 0; r--)
+	{
+		_putchar(s[r]);
+	}
+	_putchar('\n');
+}
