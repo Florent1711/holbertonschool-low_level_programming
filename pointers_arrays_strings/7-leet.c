3c5f904fa5fd488435207d26bb9cae4f1f7f286e
Florent1711 <graphit01@hotmail.com>
2023-10-28 05:32:10 -0700

    C - Pointers, arrays and strings

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+char *leet(char *s)
+{
+	int i;
+	int x;
+
+	int cherche[] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+	int remplace[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			if (s[i] == cherche[x])
+			{
+				s[i] = remplace[x / 2];
+				x = 9;
+			}
+		}
+	}
+	return (s);
+}
