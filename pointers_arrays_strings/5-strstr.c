4ae460111e1334fb4f5f52d53fd4e2951ef530a1
Florent1711 <graphit01@hotmail.com>
2023-10-29 08:00:19 -0700

    C - Pointers, arrays and strings

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int s;
+
+	s = 0;
+
+	while (needle[s] != '\0')
+		s++;
+
+	while (*haystack)
+	{
+		for (i = 0; needle[i]; i++)
+		{
+			if (haystack[i] != needle[i])
+				break;
+		}
+		if (i != s)
+			haystack++;
+		else
+			return (haystack);
+	}
+	return (0);
+}
