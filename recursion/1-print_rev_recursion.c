9f1d2c92bd4a4673e8e0e66b52d464e4fc6a67ab
Florent1711 <graphit01@hotmail.com>
2023-10-30 08:09:17 -0700

    Recursivité

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion -fonction qui inverse les caractères
+ * @s: pointeur vers la chaine
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+		_putchar(*s);
+}
