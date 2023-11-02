98f9edce2668744f0dc8f69cd032148a46f9efc1
Florent1711 <graphit01@hotmail.com>
2023-11-02 11:34:18 -0700

    C - argc, argv

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -9,9 +9,9 @@
  * Return: always 0 (success)
  */
 
-int main(int argc, __attribute__((unused)) *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
