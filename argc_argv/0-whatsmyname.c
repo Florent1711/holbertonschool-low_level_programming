4fc3751794cb7ca44d6c5eb319483c8a2a8180ff
Florent1711 <graphit01@hotmail.com>
2023-11-02 05:21:41 -0700

    C - argc, argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - affiche le nom de son propre executable
+ * @argc: nombre d'arguments
+ * @argv: table d'arguments
+ *
+ * Return: 0 (Always success)
+ */
+
 int main(int argc, char **argv)
 {
 	(void)argc;
