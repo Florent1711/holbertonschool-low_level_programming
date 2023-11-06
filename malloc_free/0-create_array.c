b29a2c22e5579386eddf332e3440490c2f7d0b22
Florent1711 <graphit01@hotmail.com>
2023-11-06 05:25:56 -0800

    C - malloc, free

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - fction crée un tableau +initialise avec un car. spécifique
+ * @size: taille du tableau
+ * @c: caractère
+ *
+ * Return: 0 ou str
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	str = malloc(sizeof(char) * size);
+
+	if (size == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
+}
