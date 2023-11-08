15223febd4262814d7cfead748f5ec25b2794f68
Florent1711 <graphit01@hotmail.com>
2023-11-08 02:30:46 -0800

    C - malloc, free

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,10 +15,10 @@ char *str_concat(char *s1, char *s2)
 	char *result = malloc(len1 + len2 + 1);
 
 	if (s1 == NULL)
-		s1 = 0;
+		s1 = '\0';
 
 	if (s2 == NULL)
-		s2 = 0;
+		s2 = '\0';
 
 	while (s1[len1] != '\0')
 		len1++;
