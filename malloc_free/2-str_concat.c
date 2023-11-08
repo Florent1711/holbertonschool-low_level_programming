f25948445b4c3abb9a95f30c3cc4ab549e18da8c
Florent1711 <graphit01@hotmail.com>
2023-11-08 01:46:45 -0800

    C - malloc, free

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int len1 = 0, len2 =0, j = 0, i = 0;
+	unsigned int len1 = 0, len2 = 0, j = 0, i = 0;
 	char *result = malloc(len1 + len2 + 1);
 
 	if (s1 == NULL)
--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * free_grid - libère une grille bidimensionnelle
+ * @grid: grille à libérer
+ * @height: hauteur
+ *
+ */
+
 void free_grid(int **grid, int height)
 {
 	int i;
