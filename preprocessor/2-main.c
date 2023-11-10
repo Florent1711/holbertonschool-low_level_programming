3236540eb1a16a02d3cfbdca11d361791b906429
Florent1711 <graphit01@hotmail.com>
2023-11-10 05:09:35 -0800

    C - Preprocessor

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - principal
+ *
+ * Description: affiche le nom du fichier à partir duquel il a été compilé
+ * Return: (0)
+ */
+
 int main(void)
 {
 printf("%s\n", __FILE__);
