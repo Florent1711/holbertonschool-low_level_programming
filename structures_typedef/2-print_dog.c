e5de1d355458b1b84334dead6f7188173ff504fb
Florent1711 <graphit01@hotmail.com>
2023-11-14 05:05:44 -0800

    C - Structures, typedef

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,6 +1,13 @@
 #include "dog.h"
 #include <stdio.h>
 
+/**
+ * print_dog - affiche une structure
+ * @d: pointeur vers la structure dog
+ *
+ * Return: Void
+ */
+
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
@@ -10,3 +17,12 @@ void print_dog(struct dog *d)
 	printf("Age: %6f\n", d->age);
 	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
+/**
+ * printf("Name: %s\n", d->name ? d->name : "(nil)");
+ *  est un ternaire égal à ce code :
+ *
+ * if (d->name != NULL)
+ *	printf("Name: %s\n", d->name);
+ * else
+ *	printf("Name: (nil)\n");
+ */
