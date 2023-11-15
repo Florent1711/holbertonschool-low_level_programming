a8c023204b0f88a8c2c754366397e5c5bb639c7c
Florent1711 <graphit01@hotmail.com>
2023-11-15 04:31:35 -0800

    C - Function pointers

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - affiche un nom
+ * @name: nom à afficher
+ * @f: pourquoi c'est ça qui imprime le nom ?
+ * Return: Null si nom ou f est null ?
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	(*f)(name);
+}
---
+++ function_pointers/README.md
@@ -0,0 +1,3 @@
+0-print_name.c - Write a function that prints a name.
+      • Prototype: void print_name(char *name, void (*f)(char *));
+
