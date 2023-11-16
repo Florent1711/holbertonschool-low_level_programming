6b668d4dc49602dd185889ab26a25061b36a9b35
Florent1711 <graphit01@hotmail.com>
2023-11-16 08:08:39 -0800

    C - Function pointers

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -5,6 +5,7 @@
  * array_iterator - execute une fonction  sur chaque élément d'un tableau
  * @size: taille du tableau
  * @array: tableau
+ * @action: pointeur sur la fonction à utiliser
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
