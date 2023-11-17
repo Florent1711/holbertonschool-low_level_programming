08300eeef35e0865b688208cc32af7499358da30
Florent1711 <graphit01@hotmail.com>
2023-11-17 13:16:28 -0800

    C - Variadic functions

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,44 +1,49 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
-
+#include <stdarg.h>
 
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	char *s;
+	/* code proposé par Bing IA */
+    int i = 0;
+    char *s;
+    va_list args;
 
-	va_list args;
+    va_start(args, format);
 
-	va_start(args, format);
-
-	while (format && format[i])
-	{
-		switch (format[i])
-		{
-		case 'c':
-			printf("%c", va_arg(args, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(args, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(args, double));
-			break;
-		case 's':
-			s = va_arg(args, char *);
-			if (s)
-				printf("%s", s);
-			else
-				printf("(nil)");
-			break;
-		default:
-			break;
-		}
-		if (format[i + 1])
-			printf(",");
-		i++;
-	}
-	printf("\n");
-	va_end(args);
+    while (format && format[i])
+    {
+        if (format[i] == 'c')
+        {
+            printf("%c", va_arg(args, int));
+        }
+        else if (format[i] == 'i')
+        {
+            printf("%d", va_arg(args, int));
+        }
+        else if (format[i] == 'f')
+        {
+            printf("%f", va_arg(args, double));
+        }
+        else if (format[i] == 's')
+        {
+            s = va_arg(args, char *);
+            if (s == NULL)
+            {
+                printf("(nil)");
+            }
+            else
+            {
+                printf("%s", s);
+            }
+        }
+        if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' ||
+                                      format[i] == 'f' || format[i] == 's'))
+        {
+            printf(", ");
+        }
+        i++;
+    }
+    printf("\n");
+    va_end(args);
 }
