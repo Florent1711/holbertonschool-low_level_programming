873f12efecff97dc41007e4bb15b16ecb881b49e
Florent1711 <graphit01@hotmail.com>
2023-11-17 13:42:32 -0800

    C - Variadic functions

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,46 +4,14 @@
 
 void print_all(const char * const format, ...)
 {
-	/* code proposé par Bing IA */
-    int i = 0;
-    char *s;
-    va_list args;
+	/* type d'argument itératifs */
+	void print_char(va_list arg)
+	{
+		char lettre;
+		/* récupère un argument */
+		lettre = va_arg(arg, int);
 
-    va_start(args, format);
+		printf("%c", lettre);
+	}
 
-    while (format && format[i])
-    {
-        if (format[i] == 'c')
-        {
-            printf("%c", va_arg(args, int));
-        }
-        else if (format[i] == 'i')
-        {
-            printf("%d", va_arg(args, int));
-        }
-        else if (format[i] == 'f')
-        {
-            printf("%f", va_arg(args, double));
-        }
-        else if (format[i] == 's')
-        {
-            s = va_arg(args, char *);
-            if (s == NULL)
-            {
-                printf("(nil)");
-            }
-            else
-            {
-                printf("%s", s);
-            }
-        }
-        if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i' ||
-                                      format[i] == 'f' || format[i] == 's'))
-        {
-            printf(", ");
-        }
-        i++;
-    }
-    printf("\n");
-    va_end(args);
-}
+	
